851fe0444c9c7e91f5cc757da964d65ff55728ca
Itzac <itzacalbertin@gmail.com>
2022-09-19 19:15:12 -0300

    added Header

---
+++ pages/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+
+export default function Header() {
+    return (
+        <header className={styles.container}>
+            <nav className={styles.navbar}>
+                <Link href='/'>
+                    <a>Home</a>
+                </Link>
+                <Link href='#'>
+                    <a>Faça seu ticket</a>    
+                </Link>   
+            </nav>
+        </header>
+    )
+}
